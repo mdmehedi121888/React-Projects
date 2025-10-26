d8d0fabf7900883e658de9342c6b0b7bf013ed11
mehedi66708 <it.wbss.soft-engineer19@waltonplc.com>
2025-10-26 11:47:43 +0600

    accordian project complete

---
+++ src/components/accordian/db.js
@@ -0,0 +1,29 @@
+const data = [
+  {
+    id: 1,
+    question: "What is the capital of France?",
+    answer: "Paris",
+  },
+  {
+    id: 2,
+    question: "Who developed the theory of relativity?",
+    answer: "Albert Einstein",
+  },
+  {
+    id: 3,
+    question: "What is the largest planet in our solar system?",
+    answer: "Jupiter",
+  },
+  {
+    id: 4,
+    question: "What is the chemical symbol for water?",
+    answer: "H2O",
+  },
+  {
+    id: 5,
+    question: "Who wrote the play 'Romeo and Juliet'?",
+    answer: "William Shakespeare",
+  },
+];
+
+export default data;
---
+++ src/components/accordian/index.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import data from "./db";
+import "./style.css";
+
+export default function Accordian() {
+  const [singleItem, setSingleItem] = useState(null);
+  const [multiItem, setMultiItem] = useState([]);
+  const [multiBtnSelect, setMutliBtnSelect] = useState(false);
+
+  function handleSingleClick(getItemId) {
+    if (multiBtnSelect) {
+      setSingleItem(null);
+      const ar = [...multiItem];
+      if (ar.includes(getItemId)) {
+        // remove the item
+        setMultiItem(ar.filter((item) => item !== getItemId));
+      } else {
+        // add the item
+        setMultiItem([...ar, getItemId]);
+      }
+    } else {
+      // single select mode
+      setSingleItem(getItemId);
+    }
+  }
+
+  function handleMultiSelectionBtn() {
+    setMutliBtnSelect(!multiBtnSelect);
+  }
+
+  return (
+    <div className="accordian-section">
+      <h1 style={{ color: "purple" }}>Accordian Example</h1>
+      <button id="multiBtn" onClick={() => handleMultiSelectionBtn()}>
+        Enable Multi Selection
+      </button>
+      {data && data.length > 0 ? (
+        data.map((dataItem, key) => {
+          return (
+            <div
+              className="container"
+              onClick={() => handleSingleClick(dataItem.id)}
+              key={dataItem.id}
+            >
+              <div className="title">{dataItem?.question}</div>
+              <span className="plusIcon">+</span>
+              {multiBtnSelect && multiItem.includes(dataItem.id) ? (
+                <div> {dataItem.answer} </div>
+              ) : singleItem && singleItem === dataItem.id ? (
+                <div> {dataItem.answer} </div>
+              ) : null}
+            </div>
+          );
+        })
+      ) : (
+        <div>Data is not found!</div>
+      )}
+    </div>
+  );
+}
